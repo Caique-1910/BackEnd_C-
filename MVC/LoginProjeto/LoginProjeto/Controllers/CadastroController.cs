7d9bfd2575c7496acab247105f7ad1baa8ce3469
CAIQUE LIMA ALVES <52918563862@info.local>
2025-11-26 09:30:52 -0300

    Continuacao dos MVCs

---
+++ MVC/LoginProjeto/LoginProjeto/Controllers/CadastroController.cs
@@ -0,0 +1,15 @@
+
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginProjeto.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
